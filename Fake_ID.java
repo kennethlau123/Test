5a8100eb52b60561bdea2be363f31c8c10103329
kennethlau123 <78078342+kennethlau123@users.noreply.github.com>
2021-01-28 11:32:02 +0800

    Update Fake_ID.java

--- Fake_ID.java
+++ Fake_ID.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.ArrayList; 
 import java.util.List; 
 import java.util.Random;
-public class Main
+public class Fake_ID
 {  
     public static void main(String args[])
     {  
